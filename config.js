07b9783ea9916b32f87ea7d968f34356e59ab000
dt09adm <dt09adm@gmail.com>
2026-09-12 14:10:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://maarvkcjuiiqevahovvf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_hSRnumIWjT_8gLClr9rSGw_a1-vSgZU";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
